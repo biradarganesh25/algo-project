e205b54f7d464917b99f2b500e53ac502233ae3a
Ganeshprasad Biradar <biradarganesh25@gmail.com>
2021-11-01 16:22:22 -0500

    Changed graph generator to return graphs with weights properly
    Initially it was returning an array of weights for each node which
    does not make sense. I was confused. Now it returns adj list of
    graph nodes which contains the vertex as well as the edge weight.

--- graph_generator.h
+++ graph_generator.h
@@ -1,3 +1,5 @@
+#ifndef GRAPHG_H
+#define GRAPHG_H
 #include <iostream>
 #include <cstdlib>
 #include <vector>
@@ -8,11 +10,21 @@ using namespace std;
 
 int sparse_graph_avg_degree = 6;
 
+class graph_node{
+    public:
+    int vertex;
+    int weight;
+    graph_node(int v, int w): vertex(v), weight(w){};
+};
+// struct complete_graph{
+//     vector<vector<graph_node>>* graph;
+// };
+
 class graph_generator{
     private: 
     int avg_degree, num_nodes, current_edges=0;
     vector<vector<int>> unique_edges;
-    vector<vector<int>>* final_graph;
+    vector<vector<graph_node>>* final_graph;
 
     public:
 
@@ -25,21 +37,22 @@ class graph_generator{
         }
         random_shuffle(unique_edges.begin(), unique_edges.end());
 
-        final_graph = new vector<vector<int>>(num_nodes, vector<int>());
+        final_graph = new vector<vector<graph_node>>(num_nodes, vector<graph_node>());
 
     }
 
     float get_avg_degree(){
-        return (float)current_edges/num_nodes;
+        return (float)2*current_edges/num_nodes;
     }
 
     void add_edge(int n1, int n2){
-        (*final_graph)[n1].push_back(n2);
-        (*final_graph)[n2].push_back(n1);
+        int w=rand()%num_nodes;
+        (*final_graph)[n1].push_back(graph_node (n2, w));
+        (*final_graph)[n2].push_back(graph_node (n1, w));
         current_edges++;
     }
 
-    vector<vector<int>>* get_graph(){
+    vector<vector<graph_node>>* get_graph(){
 
         for(int i=0;i<num_nodes;i+=1){
             add_edge(i,(i+1)%(num_nodes-1));
@@ -55,28 +68,23 @@ class graph_generator{
         return final_graph;
     }
 
-    vector<int>* get_weights(){
-        vector<int> * weights = new vector<int> (num_nodes,0);
-        for(int i=0;i<num_nodes;i++){
-            (*weights)[i] = rand()%num_nodes;
-        }
+    // vector<int>* get_weights(complete_graph cg){
+    //     vector<vector<int>> gh = *(cg.graph);
+    //     vector<int> * weights = new vector<int> (num_nodes,0);
+    //     for(int i=0;i<num_nodes;i++){
+    //         (*weights)[i] = rand()%num_nodes;
+    //     }
 
-        return weights;
-    }
+    //     return weights;
+    // }
 
 };
 
-struct complete_graph{
-    vector<vector<int>>* graph;
-    vector<int> *weights;
-};
 
-complete_graph get_graph(int degree, int num_nodes){
+vector<vector<graph_node>>* get_graph(int degree, int num_nodes){
+
     graph_generator gh(degree, num_nodes);
-    complete_graph cg;
-    cg.graph = gh.get_graph();
-    cg.weights = gh.get_weights();
-
-    return cg;
+    return gh.get_graph();
 }
 
+#endif
--- test_graph_generator.cpp
+++ test_graph_generator.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
-#include "graph_generator.cpp"
+#include "graph_generator.h"
 
 using namespace std;
 
 int main(){
     int degree=6, num_nodes=10;
-    complete_graph cg = get_graph(degree, num_nodes);
-    vector<vector<int>> gh = (*cg.graph);
+    vector<vector<graph_node>>* gh_ptr= (get_graph(degree, num_nodes));
+    vector<vector<graph_node>>& gh= *(gh_ptr);
+
 
     int total_deg=0;
     vector<int> degrees(num_nodes);
@@ -19,16 +20,20 @@ int main(){
     cout<<"Avg degree: "<<total_deg/num_nodes<<endl;
     cout<<"Degrees: "<<endl;
     for(int i=0;i<num_nodes;i++){
-        cout<<degrees[i]<<" ";
-    }
-    cout<<endl;
-    cout<<"Graph adj list: "<<endl;
-    for(int i=0;i<num_nodes;i++){
-        cout<<i<<": ";
-        for(int neigh:gh[i]){
-            cout<<neigh<<" ";
+        cout<<i<<":: ";
+        for(int j=0;j<gh[i].size();j++){
+            cout<<gh[i][j].vertex<<":w"<<gh[i][j].weight<<" ";
         }
-    cout<<endl;
+        cout<<endl;
     }
+    // cout<<endl;
+    // cout<<"Graph adj list: "<<endl;
+    // for(int i=0;i<num_nodes;i++){
+    //     cout<<i<<": ";
+    //     for(int neigh:gh[i]){
+    //         cout<<neigh<<" ";
+    //     }
+    // cout<<endl;
+    // }
 }
 
